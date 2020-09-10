a191f5746ed925633c9ad91d4bddf50b048f3309
Your Name <you@example.com>
2020-09-10 12:08:55 +0200

    main.ts -> bootstrap -> NgModule -> ngDoBootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hoist-angular';
 }
+
+
+// console.log(AppComponent)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, DoBootstrap, ApplicationRef } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -15,4 +15,11 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule  {}
+// export class AppModule implements DoBootstrap {
+
+//   ngDoBootstrap(appRef: ApplicationRef) {
+//     // getConfigFromAPI.then( () => ...
+//     appRef.bootstrap(AppComponent, 'root-app')
+//   }
+// }
