453b37ba97b43f4f53570c5ad6f3fb71c2ff3ecb
Your Name <you@example.com>
2020-09-11 14:30:05 +0200

    Input / Output - one way data flow - 2 way binding

--- src/app/playlists/components/playlist-details/playlist-details.component.ts
+++ src/app/playlists/components/playlist-details/playlist-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Playlist } from 'src/app/core/models/playlist';
 
 
@@ -9,6 +9,7 @@ import { Playlist } from 'src/app/core/models/playlist';
 })
 export class PlaylistDetailsComponent implements OnInit {
 
+  @Input()
   playlist: Playlist = {
     id:123,
     name: 'pancakes',
--- src/app/playlists/components/playlist-list/playlist-list.component.ts
+++ src/app/playlists/components/playlist-list/playlist-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 import { Playlist } from 'src/app/core/models/playlist';
 import { NgForOfContext, NgForOf } from '@angular/common';
 
@@ -11,43 +11,33 @@ NgForOfContext
   styleUrls: ['./playlist-list.component.scss'],
   // encapsulation: ViewEncapsulation.ShadowDom
   // encapsulation: ViewEncapsulation.Emulated
+  // inputs: [
+  //   'playlists:items'
+  // ]
 })
 export class PlaylistListComponent implements OnInit {
 
-  playlists: Playlist[] = [
-    {
-      id: 123,
-      name: 'pancakes 1',
-      public: true,
-      description: 'Test'
-    },
-    {
-      id: 234,
-      name: 'pancakes 2',
-      public: true,
-      description: 'Test'
-    },
-    {
-      id: 345,
-      name: 'pancakes 3',
-      public: true,
-      description: 'Test'
-    },
-  ]
-  
-  selected:Playlist = this.playlists[0]
+  @Input('items')
+  playlists: Playlist[] = []
+
+  selected: Playlist | null = null;
+
+  @Output('selectedChange')
+  selectedChange = new EventEmitter<Playlist>()
 
   select(playlist: Playlist) {
-    this.selected =  playlist
+    // this.selectedChange.subscribe(listner)
+    this.selectedChange.emit(playlist)
+    this.selected = playlist
   }
 
   constructor() { }
 
   ngOnInit(): void {
-    setInterval(()=>{
-      if(this.playlists.length)
-        this.playlists.unshift(this.playlists.pop()!)
-    },1000)
+    // setInterval(()=>{
+    //   if(this.playlists.length)
+    //     this.playlists.unshift(this.playlists.pop()!)
+    // },1000)
   }
 
 }
