3e84c370371eb8ea4bd860da61c9b025515cee83
Your Name <you@example.com>
2020-09-18 11:13:50 +0200

    take, takeWhile, takeUntil( signal )

--- src/app/music-search/containers/album-search/album-search.component.ts
+++ src/app/music-search/containers/album-search/album-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
+import { takeUntil, takeWhile, tap } from 'rxjs/operators';
 import { Album } from 'src/app/core/models/album';
 import { MusicSearchService } from 'src/app/core/services/music-search.service';
 
@@ -22,22 +22,26 @@ export class AlbumSearchComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.sub
-      .add(this.service.getAlbumsUpdates()
-        .subscribe({
-          next: albums => this.results = albums,
-          error: error => this.message = error.message,
-        }))
-      .add(this.service.query.subscribe({
+
+    this.service.getAlbumsUpdates()
+      .pipe(takeUntil(this.destroySignal))
+      .subscribe({
+        next: albums => this.results = albums,
+        error: error => this.message = error.message,
+      })
+
+    this.service.query
+      .pipe(takeUntil(this.destroySignal))
+      .subscribe({
         next: query => {
           this.query = query
         }
-      }))
+      })
   }
 
-  sub = new Subscription()
+  destroySignal = new Subject()
 
   ngOnDestroy() {
-    this.sub.unsubscribe()
+    this.destroySignal.next()
   }
 }
